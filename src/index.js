878eb0d8ef94044044b99591fe0e6ae0d8b45a03
Uzair169 <96523038+Uzair169@users.noreply.github.com>
2024-09-02 14:54:30 +0500

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,76 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import Image from './images.jpeg'
+import './styles.css'
+import twitter from './twitter.png'
+import facebook from './facebook.webp'
+import instagram from './Instagram.png'
+import github from './github.webp'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function MainPicture() {
+    return (
+        <img src={Image} alt="Image" className="img-container"/>
+    )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Intro() {
+    return (
+        <div className="div-intro">
+            <h2>Bill Gates</h2>
+            <h4>Founder, Microsoft</h4>
+            <p>microsoft.com</p>
+            <div className="buttons">
+                <button className="email">Email</button>
+                <button className="linkedin">LinkedIn</button>
+            </div>
+        </div>
+    )
+}
+
+function About() {
+    return(
+        <div className="about">
+            <h3>About</h3>
+            <p>I am a frontend developer with a particular interest in making things simple and automating daily tasks. I try to keep up with security and best practices, and am always looking for new things to learn.</p>
+        </div>
+    )
+}
+
+function Interests() {
+    return(
+        <div className="interests">
+            <h3>Interests</h3>
+            <p>Food expert. Music scholar. Reader. Internet fanatic. Bacon buff. Entrepreneur. Travel geek. Pop culture ninja. Coffee fanatic.</p>
+        </div>
+    )
+}
+
+function Footer() {
+    return(
+        <div className="footer">
+            <img src={twitter} alt="twitter" className="logos"/>
+            <img src={facebook} className="logos"/>
+            <img src={instagram} className="logos"/>
+            <img src={github} className="logos"/>
+        </div>
+    )
+}
+
+function App() {
+    return (
+        <div className="main-container">
+
+            <div className="main-div">
+            <MainPicture />
+            <Intro />
+            <About />
+            <Interests />
+            <Footer />
+            </div>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<App />)
